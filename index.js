ca8a48d28f9387d1ff0e1afac19dfd3c78c92bb7
PhamSangPS <160474426+PhamSangPS@users.noreply.github.com>
2025-02-09 16:00:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan())
 app.use(helmet({
-    crossOriginResourcePolicy : true
+    crossOriginResourcePolicy : false
 }))
 
 const PORT = 8080 || process.env.PORT 
