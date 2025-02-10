466fe7029cd13b32ba0937d4340ce7a3abe89acc
PhamSangPS <160474426+PhamSangPS@users.noreply.github.com>
2025-02-10 22:58:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import orderRouter from './route/order.route.js'
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : 'https://www.dientromt.com/',
     credentials : true,
 }))
 app.use(express.json())
